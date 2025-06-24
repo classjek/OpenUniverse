6f06c5ea1348deefc454a242494d696c9668bc3e
Jake Ekoniak <jakee@node22.engr.wustl.edu>
2025-06-24 13:09:14 -0500

    Set up mechanism to query YAGO3-10 database via DuckDB with our program. Provide sample query in main.

---
+++ gen_pop/src/main.cpp
@@ -0,0 +1,46 @@
+#include "duckdb.hpp"
+#include <iostream>
+#include <vector>
+#include <filesystem>
+
+int main(){
+    using namespace duckdb; 
+    namespace fs = std::filesystem;
+    
+    std::cout << "Hello there guy, " << __cplusplus << std::endl;
+    std::cout << "cwd  = " << fs::current_path() << '\n';
+
+    fs::path parquet = fs::current_path() / "../data/yago3-10.parquet";
+    std::cout << "parquet path = " << parquet << '\n';
+    if (!fs::exists(parquet)) {
+        std::cerr << "ERROR: Parquet file not found at that path\n";
+        return 1;
+    }
+
+    // Start in-memory DuckDB instance
+    DuckDB db(nullptr);  //  pass nullptr so DB doesn't persist on disk
+    Connection conn(db);
+
+    conn.Query("INSTALL parquet;");
+    conn.Query("LOAD parquet;"); 
+
+    std::string sql =
+    "CREATE OR REPLACE TABLE yago AS "
+    "SELECT * FROM read_parquet('" + parquet.string() + "');";
+    auto res = conn.Query(sql);
+    if (res->HasError()) {
+        std::cerr << res->GetError() << '\n';
+        return 1; 
+    }
+
+    auto head = conn.Query("SELECT * FROM yago LIMIT 10;");
+    for (auto &name : head->names) std::cout << name << '\t';
+    std::cout << '\n' << '\n';
+    for (auto &row : *head) {
+        for (idx_t i = 0; i < head->ColumnCount(); ++i)
+            std::cout << row.GetValue<std::string>(i) << '\t';
+        std::cout << '\n';
+    }
+
+    return 0;
+}
